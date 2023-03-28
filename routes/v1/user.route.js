bdd559fd1f948755097a1ad343406f6c98d992cb
siamalrafi <rahathasandipto@gmail.com>
2023-03-28 21:14:26 +0600

    hellow

---
+++ routes/v1/user.route.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+router.route("/")
+ /**
+   * @api {get} /tools All tools
+   * @apiDescription Get all the tools
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+
+
+.get((req,res)=>{
+    res.send("main get route hits. ")
+});
+
+
