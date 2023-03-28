7318405f21bf97b8e8c8b2d2fbaac627c2adf29a
siamalrafi <rahathasandipto@gmail.com>
2023-03-28 21:48:14 +0600

    errorHandler

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 const router = require('./routes/v1/test.route');
-const port = process.env.PORT || 5000;
+const errorHandler = require("./middleware/errorHandler"); 
 
 
 
@@ -13,9 +14,13 @@ app.use(express.json());
 
 
 
-app.use("/api/v1/test",router);
+app.use("/api/v1/test", router);
 
+app.all("*", (req, res) => {
+  res.send("All router")
+});
 
+app.use(errorHandler);
 
 
 app.listen(port, () => {
--- routes/v1/test.route.js
+++ routes/v1/test.route.js
@@ -4,26 +4,26 @@ const router = express.Router();
 
 
 router.route("/")
- /**
-   * @api {get} /tools All tools
-   * @apiDescription Get all the tools
-   * @apiPermission admin
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiParam  {Number{1-}}         [page=1]     List page
-   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
-   *
-   * @apiSuccess {Object[]} all the tools.
-   *
-   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
-   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
-   */
+  /**
+    * @api {get} /tools All tools
+    * @apiDescription Get all the tools
+    * @apiPermission admin
+    *
+    * @apiHeader {String} Authorization   User's access token
+    *
+    * @apiParam  {Number{1-}}         [page=1]     List page
+    * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+    *
+    * @apiSuccess {Object[]} all the tools.
+    *
+    * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+    * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+    */
 
 
-.get((req,res)=>{
+  .get((req, res) => {
     res.send("Test routed checked!")
-});
+  });
 
 
 
