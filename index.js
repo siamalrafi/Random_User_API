1631a5b56b48848d1cea544097b7fc0fa314e6be
siamalrafi <rahathasandipto@gmail.com>
2023-03-28 21:08:00 +0600

    hello

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
